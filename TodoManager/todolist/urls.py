410e5dd14ab6312db4d66f496ecfb6328d4943ba
ajay <ajaykumar290904@gmail.com>
2026-02-27 01:28:57 +0530

    Update URL patterns, add new templates for About and Contact pages, and modify homepage title

--- TodoManager/todolist/urls.py
+++ TodoManager/todolist/urls.py
@@ -3,7 +3,7 @@ from todolist import views
 
 urlpatterns = [
     path('',views.hompage,name="homepage"),
-    path('todolist',views.todolist,name="todolist"),
-    path('contact',views.contact,name="todolist"),
-    path('about',views.about,name="todolist"), 
+    path('todolist/',views.todolist,name="todolist"),
+    path('contact/',views.contact,name="contact"),
+    path('about/',views.about,name="about"), 
 ] 
