fd9f1dc1bc469d4293f079cb79b335d51c3c13d9
ajay <ajaykumar290904@gmail.com>
2026-02-27 01:19:30 +0530

    Update URL patterns and main.html structure for TodoManager app

--- TodoManager/todolist/urls.py
+++ TodoManager/todolist/urls.py
@@ -2,5 +2,8 @@ from django.urls import path,include
 from todolist import views
 
 urlpatterns = [
-    path('todolist',views.todolist,name="todolist")
+    path('',views.todolist,name="homepage"),
+    path('todolist',views.todolist,name="todolist"),
+    path('contact',views.todolist,name="todolist"),
+    path('about',views.todolist,name="todolist"), 
 ]
