e30dde5bc569bb23e8e81e97b0dd741eb5907bde
ajay <ajaykumar290904@gmail.com>
2026-03-01 13:44:59 +0530

    Implement TaskForm handling in todolist view for task creation

--- todolist/views.py
+++ todolist/views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse,JsonResponse
 from todolist.models import Task
+from todolist.forms import TaskForm
 
 def hompage(request):
   
     return render(request, "main.html", {})
 
+
 def todolist(request):
+    if request.method == "POST":
+       form_data = TaskForm(request.POST or None)
+       if form_data.is_valid():
+        form_data.save()
+        return redirect('todolist')
     all_task = Task.objects.all()
     context = {
         'page':'todolist',
