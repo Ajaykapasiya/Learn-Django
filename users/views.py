e53858c5b63953464ecbde7049bd7eb82b5cfc91
ajay <ajaykumar290904@gmail.com>
2026-03-03 18:56:11 +0530

    Enhance registration page layout and functionality with improved styling and user feedback

--- users/views.py
+++ users/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def register(request):
     if request.method == "POST":
-        pass
+        register_form = UserCreationForm(request.POST)
+        if register_form.is_valid():
+            register_form.save() 
+            messages.success(request,"User Created Successfully!")
+            return redirect("todolist")
+        
     else:
-        pass
-    register_form  = UserCreationForm() 
+        register_form  = UserCreationForm() 
     return render(request, 'register.html',{'register_form':register_form})
