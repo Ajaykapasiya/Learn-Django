6981523bdcb423e9f78732015bf8a4cd8f5e3a8e
ajay <ajaykumar290904@gmail.com>
2026-03-03 17:42:17 +0530

    Implement user registration form with CSRF token and submit button

--- users/views.py
+++ users/views.py
@@ -3,5 +3,9 @@ from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
+    if request.method == "POST":
+        pass
+    else:
+        pass
     register_form  = UserCreationForm() 
     return render(request, 'register.html',{'register_form':register_form})
