e83db65d533086ae0f1b80715b2f4c2e62ccd9c5
ajay <ajaykumar290904@gmail.com>
2026-03-05 12:55:26 +0530

    Refactor registration form layout and remove redundant button

--- users/forms.py
+++ users/forms.py
@@ -6,6 +6,9 @@ from django.contrib.auth.forms import UserCreationForm
 class CustomRegistrationForm(UserCreationForm):
     email = forms.EmailField()
     
-class Meta:
-    model=User
-    fields = ['username', 'email', 'password1', 'password2']
+    class Meta:
+        model=User
+        fields = ['username', 'email', 'password1', 'password2']
+    
+    
+    
