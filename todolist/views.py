cc53dbd70c3e847ef72654ec8cb6111d1819a381
ajay <ajaykumar290904@gmail.com>
2026-03-06 01:09:11 +0530

    Add sign-up button to base template and enforce login requirement for todolist view

--- todolist/views.py
+++ todolist/views.py
@@ -4,12 +4,13 @@ from todolist.models import Task
 from todolist.forms import TaskForm
 from django.contrib import messages 
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 def hompage(request):
   
     return render(request, "main.html", {})
 
-
+@login_required
 def todolist(request):
     if request.method == "POST":
        form_data = TaskForm(request.POST or None)
