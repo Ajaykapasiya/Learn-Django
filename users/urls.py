87ae003a2b9bc634152e80e6669bb66b5c2c149b
ajay <ajaykumar290904@gmail.com>
2026-03-05 13:29:01 +0530

    Add login URL path and create login template with crispy forms integration

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path,include
 from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-  path("register/", user_views.register,name="register")
+  path("register/", user_views.register,name="register"),
+  path("login/", auth_views.LoginView.as_view(),name="login")
+
 ] 
