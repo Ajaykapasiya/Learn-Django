960d48c54b64c4d2c87398a6ed6b8ccc4fd2c557
ajay <ajaykumar290904@gmail.com>
2026-03-05 14:13:38 +0530

    Add logout URL path to user URL configuration

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
   path("register/", user_views.register,name="register"),
-  path("login/", auth_views.LoginView.as_view(template_name="login.html"),name="login")
+  path("login/", auth_views.LoginView.as_view(template_name="login.html"),name="login"),
+  path("logout/", auth_views.LogoutView.as_view(),name="logout")
+
 
 ] 
